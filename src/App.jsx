45372b7360fbb7c6de635d1a336f49a1220f57ba
yonsu-git <sapo85987@gmail.com>
2026-01-02 21:53:04 +0900

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+function App() {
+  return (
+    <main style={{ maxWidth: 800, margin: "40px auto", fontFamily: "sans-serif"}}>
+      <h1>Inagaki Hirotoのホームページ</h1>
+      <p>Programmingができるようになりたい。</p>
+
+      <section>
+        <h2>About</h2>
+        <p>
+          計算化学・Python・機械学習に興味があります。
+        </p>
+      </section>
+
+      <section>
+        <h2>Links</h2>
+        <ul>
+          <li><a href="https;//github.com/">Github</a></li>
+        </ul>
+      </section>
+    </main>
+  );
+}
+
+export default App;
+
