fb8c0f28a2abcccff21702354e45d32bc67b2872
yonsu-git <sapo85987@gmail.com>
2026-01-03 00:15:59 +0900

    UIの変更 1回目

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
-
+import { FaGithub } from "react-icons/fa";
+import { FaXTwitter} from "react-icons/fa6"
 function App() {
   return (
-    <main style={{ maxWidth: 800, margin: "40px auto", fontFamily: "sans-serif"}}>
+    <div className="container">
+      <div className="card">
       <h1>Inagaki Hirotoのホームページ</h1>
       <p>Programmingができるようになりたい。</p>
+      </div>
 
-      <section>
+      <div className="card">
         <h2>About</h2>
         <p>
           計算化学・Python・機械学習に興味があります。
         </p>
-      </section>
+      </div>
 
-      <section>
+      <div className="card">
         <h2>Links</h2>
-        <ul>
-          <li><a href="https;//github.com/">Github</a></li>
+        <ul className="link">
+          <li><a href="https://github.com/"><FaGithub /></a></li>
+          <li><a href="https://x.com/home?lang=ja"><FaXTwitter /></a></li>
         </ul>
-      </section>
-    </main>
+      </div>
+    </div>
   );
 }
 
